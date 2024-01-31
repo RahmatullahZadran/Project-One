2e73e4b3e978661010d601500f4bc5753cbb0c4d
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-01-31 19:37:59 +0000

    jquery added

--- Coding/Javascript.js
+++ Coding/Javascript.js
@@ -8,3 +8,7 @@ function openPopup() {
   function closePopup() {
     document.getElementById('overlay').style.display = 'none';
   }
+  $('#overlay').append('<h1>Hello World</h1>');
+  $('#overlay').click(closePopup);
+  $('#closePopup').click(closePopup);
+  
